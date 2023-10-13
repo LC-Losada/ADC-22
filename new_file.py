c7d102d110c711fc232a4d31db795a7f5b21bf43
SST0RMY <147820311+SST0RMY@users.noreply.github.com>
2023-10-13 12:24:50 +0100

    Update new_file.py

--- new_file.py
+++ new_file.py
@@ -1,3 +1,16 @@
 
 print(Tá quieto Stick La Pisse)
 
+katota = input("Seleciona o tipo de Katota q tu gostas ("Farfalhuda", "Inchada", "Obesa", "Escancarada", Seleciona com as inicias:)")
+
+if katota = 'F' or katota = 'f':
+  print("Ganda bixo!")
+if katota = 'I' or katota = 'i':
+  print("Gostas da catota gorda!")
+if katota = 'O' or katota = 'o':
+  print("Que nojento da merda!")
+if katota = 'E' or katota = 'e':
+  print("Gostas de uma boa mina!")
+  
+
+
