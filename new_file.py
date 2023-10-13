42eff118188ffb63c5be490af48380e44e9dcd33
NidusAddict <a85211@gmail.com>
2023-10-13 18:43:45 +0100

    Arranjar a tralha que o martim faz

--- new_file.py
+++ new_file.py
@@ -1,16 +1,17 @@
 
-print(Tá quieto Stick La Pisse)
+print("Tá quieto Stick La Pisse")
 
 katota = input("Seleciona o tipo de Katota q tu gostas ("Farfalhuda", "Inchada", "Obesa", "Escancarada", Seleciona com as inicias:)")
 
-if katota = 'F' or katota = 'f':
+if katota == 'F' or katota == 'f':
   print("Ganda bixo!")
-if katota = 'I' or katota = 'i':
+if katota == 'I' or katota == 'i':
   print("Gostas da catota gorda!")
-if katota = 'O' or katota = 'o':
+if katota == 'O' or katota == 'o':
   print("Que nojento da merda!")
-if katota = 'E' or katota = 'e':
+if katota == 'E' or katota == 'e':
   print("Gostas de uma boa mina!")
   
+
 
 
